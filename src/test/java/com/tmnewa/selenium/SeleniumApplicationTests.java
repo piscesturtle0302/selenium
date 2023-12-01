dd0bd5dc282b2d7f4ff8f5462e0889b25445ec54
piscesturtle0302 <piscesturtle@gmail.com>
2023-12-01 11:26:24 +0800

    new fail test

--- src/test/java/com/tmnewa/selenium/SeleniumApplicationTests.java
+++ src/test/java/com/tmnewa/selenium/SeleniumApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,15 +17,22 @@ class SeleniumApplicationTests {
 
 	@Test
 	public void testContent() throws Exception{
-		WebDriverManager.chromedriver().setup();//啟動chromedriver
+		//WebDriverManager.chromedriver().setup();//啟動chromedriver
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pisce\\Downloads\\chrome-win64\\chrome-win64\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();//新增chromedriver物件
 
-		driver.get("http://localhost:8080/IDNOcreator/");//使用chromedirver取得網頁
+		driver.get("http://localhost:8080/");//使用chromedirver取得網頁
 		String title = driver.getTitle();//取得網頁TITLE
 		System.out.println("-----this test title : " + title);
 
-		WebElement createBtn = driver.findElement(By.id("creatNewResidenceIdNoBtn"));//抓取網頁元素
-		createBtn.click();//進行該元素動作
+		//符合規則
+//		WebElement createBtn = driver.findElement(By.id("creatNewResidenceIdNoBtn"));//抓取網頁元素
+//		createBtn.click();//進行該元素動作
+
+		//不符合規則
+		WebElement inputText = driver.findElement(By.id("checkNewResidenceIdNo"));
+		inputText.sendKeys("A123456789");
+		inputText.sendKeys(Keys.ENTER);
 
 		Thread.sleep(2000);
 
